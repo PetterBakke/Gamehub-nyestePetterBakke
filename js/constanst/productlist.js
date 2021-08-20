be4aeda903524b92255ef88d9540c183b8da0c0d
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2021-08-20 10:41:02 +0200

    Add files via upload

---
+++ js/constanst/productlist.js
@@ -0,0 +1,23 @@
+export const productArray = [
+  {
+    name: "Battlefield V",
+    description: "Battlefield V PS4",
+    id: "1",
+    image: "Battlefield v logo.jpg",
+    price: 30,
+  },
+  {
+    name: "Grand Theft Auto V",
+    description: "GTA 5 PS4 game",
+    id:"2",
+    image: "GTA 5 Logo cover.jpg",
+    price: 20,
+  },
+  {
+    name: "Red dead redemption 2",
+    description: "Red Dead Redemption PS4 game",
+    id: "3",
+    image: "Red dead redemption logo cover.jpg",
+    price: 25,
+  },
+];
